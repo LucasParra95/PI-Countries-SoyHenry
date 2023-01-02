05b69089d1f74553df9e6986c5123a4491c80321
LucasParra95 <lucasandresparra@gmail.com>
2023-01-02 18:49:37 -0300

    Funcionando3

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import Landing from './components/Landing';
@@ -19,14 +18,6 @@ function App() {
         </Switch>
       </div>
     </BrowserRouter>
-=======
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
->>>>>>> 28eee10edae3e070dabba76d4a5206704cfbb72c
   );
 }
 
